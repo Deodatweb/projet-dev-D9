abfc8e7aee98c2072a1f6094cbd669425982c6a7
Deodatweb <deodatweb@gmail.com>
2025-04-21 06:38:58 +0200

    mise à jour app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: "app-root",
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <body>
+      <app-header />
+      <router-outlet />
+      <app-footer />
+    </body>
   `,
-  styles: [],
+  styles: [
+    `
+      body {
+        background-color: #f9d0a3;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'projet-dev-D9';
+  title = "projet-dev-D9";
 }
