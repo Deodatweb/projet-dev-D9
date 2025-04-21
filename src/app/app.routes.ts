84eabb68df67e011707f5d92beb93e49d3572a7d
Deodatweb <deodatweb@gmail.com>
2025-04-21 06:27:04 +0200

    Structure du router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ProductComponent } from "./components/product/product.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
+  {
+    path: "contact",
+    component: ContactComponent,
+  },
+  {
+    path: "product/:id",
+    component: ProductComponent,
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
+  },
+];
