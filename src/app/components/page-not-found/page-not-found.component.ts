73891a8a5a728ea98c7a0901ca0cf372151c11e0
Deodatweb <deodatweb@gmail.com>
2025-04-21 07:41:37 +0200

    Component Page-not-found

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-page-not-found',
-  imports: [],
+  selector: "app-page-not-found",
+  imports: [RouterLink],
   template: `
+    <h2>Page 404 – Intrus détecté chez les Gaulois !</h2>
     <p>
-      page-not-found works!
+      Ouille ! Même Obélix ne retrouve pas cette page… Elle a dû se perdre en
+      allant affronter les Romains.<br />
+      Pas de panique, tu peux retourner à l’accueil ou explorer les autres
+      sentiers du village !
     </p>
+    <a routerLink="/">Retour à l'accueil</a>
   `,
-  styles: ``
-})
-export class PageNotFoundComponent {
+  styles: `
+    /* ===== Css Global ===== */
+    h2 {
+      color: #DA001E;
+    }
 
-}
+    p {
+      font-family: 'Comfortaa', sans-serif;
+    }
+
+    a {
+      text-decoration: none;
+      color: #DA001E;
+    }
+  
+  `,
+})
+export class PageNotFoundComponent {}
